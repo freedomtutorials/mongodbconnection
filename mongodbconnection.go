f0f2e3fdba4bf6f7c27d00eb945f19a9f831284a
Anil Pashikanti <anilflash@gmail.com>
2022-04-30 16:33:57 +0530

    Add files via upload

---
+++ mongodbconnection.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	
+	// set the mongodb uri 
+	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+	
+	//connect to mongodb
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping(context.TODO(), nil)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Connected to MongoDB!")
+}
